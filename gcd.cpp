de907d064d90e120bf008ce16a34746505052cc4
yanametro <yanametro@213.180.218.91-red.dhcp.yndx.net>
2016-10-27 15:36:41 +0300

    gcd.cpp added

---
+++ gcd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int gcd(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+
+	return gcd(b, a % b);
+}
+
+
+int main() {
+	return 0;
+}
